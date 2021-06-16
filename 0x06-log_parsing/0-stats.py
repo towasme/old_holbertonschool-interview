7048db3e40eebdbb167577f64c48685d4a15ac40
Tomas <towasm@gmail.com>
2021-06-15 23:21:47 -0500

    adding new code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,41 +5,23 @@
 import sys
 
 
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
+res = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+       "405": 0, "500": 0}
 size = 0
-
 try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    for i, logs in enumerate(sys.stdin, 1):
+        data = logs.split()
+        if len(data) > 2:
+            size += int(data[-1])
+            if data[-2] in res.keys():
+                res[data[-2]] += 1
+            if i % 10 == 0:
+                print("{}: {}".format("File size", size))
+                for key in sorted(res.keys()):
+                    if res[key] != 0:
+                        print("{}: {}".format(key, res[key]))
+finally:
+    print("{}: {}".format("File size", size))
+    for key in sorted(res.keys()):
+        if res[key] != 0:
+            print("{}: {}".format(key, res[key]))
